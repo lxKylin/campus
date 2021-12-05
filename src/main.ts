a144b3ebc51e72e3ce8d5a4d3c88084460f66502
kylin <liux1118@qq.com>
2021-12-05 11:25:07 +0800

    集成echarts,dataV等

--- src/main.ts
+++ src/main.ts
@@ -1,13 +1,21 @@
 import { createApp } from 'vue'
 
+// css初始化
+import 'normalize.css'
+import './assets/css/index.less'
+
 import App from './App.vue'
 
 import router from './router'
 import store from './store'
 
+import dataV from '@jiaminghi/data-view'
+
+
 
 const app = createApp(App)
 
+app.use(dataV)
 // 注册vuex
 app.use(store)
 
