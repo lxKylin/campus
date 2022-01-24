e5325532debe1bad36e71b22ffbfc2870729bbf7
kylin <liux1118@qq.com>
2022-01-24 16:34:52 +0800

    搭架子

---
+++ src/pages/main/overview/overview.vue
@@ -0,0 +1,83 @@
+<template>
+  <div class="work">
+    <el-row class="work__row" :gutter="5">
+      <el-col class="work__row__col" :span="5">
+        <lx-card class="card" title="生源各项比例">
+          <water-polo-echart />
+        </lx-card>
+      </el-col>
+      <el-col class="work__row__col" :span="14">
+        <lx-card-two class="card" title="FIT生源分布/毕业去向">
+          <map-echart />
+        </lx-card-two>
+      </el-col>
+      <el-col class="work__row__col" :span="5">
+        <lx-card class="card" title="毕业生职业云图">
+          <WordCloudWork />
+        </lx-card>
+      </el-col>
+    </el-row>
+    <el-row class="work__row" :gutter="5">
+      <!-- <el-col class="work__row__col" :span="4">
+        <lx-card class="card" title="书籍借阅云图">
+          <word-cloud-echart class="echart" />
+        </lx-card>
+      </el-col> -->
+      <el-col class="work__row__col" :span="16">
+        <lx-card class="card" title="各行业就业分布">
+          <line-echart />
+        </lx-card>
+      </el-col>
+      <el-col class="work__row__col" :span="8">
+        <lx-card class="card" title="活动公告">
+
+        </lx-card>
+      </el-col>
+    </el-row>
+  </div>
+</template>
+
+<script lang="ts">
+import { defineComponent, ref } from 'vue'
+
+import {LxCard, LxCardTwo} from '@/base-ui/card'
+
+import { PieEchart, WordCloudEchart, LineEchart } from '@/components/library-echarts'
+import ScrollTable from '@/components/scroll-table.vue'
+import { MapEchart, WaterPoloEchart, WordCloudWork } from '@/components/work-echarts'
+
+export default defineComponent({
+  components: {
+    LxCard,
+    LxCardTwo,
+    PieEchart,
+    ScrollTable,
+    WordCloudEchart,
+    LineEchart,
+    MapEchart,
+    WaterPoloEchart,
+    WordCloudWork
+  },
+
+  setup() {
+    const pieData = ref()
+    return {
+      pieData
+    }
+  }
+})
+</script>
+
+<style lang="less" scoped>
+.work {
+  &__row {
+    margin-bottom: 20px;
+    &__col {
+      & .card {
+      }
+      & .echart {
+      }
+    }
+  }
+}
+</style>
---
+++ src/router/main/work/work.ts
@@ -0,0 +1,7 @@
+const work = () => import('@/pages/main/work/work.vue')
+export default {
+  path: '/main/work',
+  name: 'work',
+  component: work,
+  children: []
+}
